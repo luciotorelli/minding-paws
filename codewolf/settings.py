6f36d8be7f128a5e7407972aca480cf2baccd918
Lúcio Torelli <luciotorelli@hotmail.com>
2023-07-14 15:33:30 +0100

    Update .gitignore and connect to ElephantSQL

--- codewolf/settings.py
+++ codewolf/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +31,6 @@ SECRET_KEY = 'django-insecure-gaky76aaq36e!z0%)4c0u)kko^8yx58d#bx@epn50zk$bcbfah
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -74,11 +78,15 @@ WSGI_APPLICATION = 'codewolf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
