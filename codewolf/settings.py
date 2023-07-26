63dde4b33b21dc188217dd15c5c4d93330577e14
Lúcio Torelli <luciotorelli@hotmail.com>
2023-07-26 15:56:49 +0100

    Make debug variable boolean

--- codewolf/settings.py
+++ codewolf/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-gaky76aaq36e!z0%)4c0u)kko^8yx58d#bx@epn50zk$bcbfah'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['minding-paws-9dfff64ed9cc.herokuapp.com',
                  '127.0.0.1', '.ngrok-free.app']
