16cc7c25438178367613aade6a1d6a5dd93c5f41
Lúcio Torelli <luciotorelli@hotmail.com>
2023-08-01 20:35:27 +0100

    Clear the selected minder on storage session
    + before saving a new one

--- static/js/selected_minder.js
+++ static/js/selected_minder.js
@@ -1,6 +1,11 @@
 // selected_minder.js
 
 function saveToSessionStorage(minderUser, minderId, minderName, minderPhotoUrl, minderAvailability) {
+
+  // Clear the Minder before saving
+  sessionStorage.clear();
+
+  // Store the minder details
   sessionStorage.setItem("minderUser", minderUser);
   sessionStorage.setItem("minderId", minderId);
   sessionStorage.setItem("minderName", minderName);
