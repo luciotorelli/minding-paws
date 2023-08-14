565bcff353d9c0050dc5b196a6a94d1941a8051e
Lúcio Torelli <luciotorelli@hotmail.com>
2023-08-14 23:20:08 +0100

    Use different database to run automated tests

--- codewolf/settings.py
+++ codewolf/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import sys
 if os.path.isfile('env.py'):
     import env
 
@@ -117,6 +118,10 @@ WSGI_APPLICATION = 'codewolf.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+# Use different database to run tests.
+if 'test' in sys.argv or 'test_coverage' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 
 # Password validation
